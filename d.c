2d48a27864d07de872670a287da7d69d44c7e61a
kumaransabapathy <31533930+kumaransabapathy@users.noreply.github.com>
2018-01-19 11:15:09 +0000

    Create d.c

---
+++ d.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a,i,n,sum=0;
+scanf("%d",&a);
+i=a;
+n=i;
+while(n!=0)
+{
+i=n%10;
+sum=sum+(i*i*i);
+n=n/10;
+}
+if(a==sum)
+printf("amstrong num");
+else
+printf("not an amstrong num");
+}
+return 0;
+}
