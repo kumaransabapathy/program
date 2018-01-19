1d91bc5006a236583aaa590596a113dbfce4d0aa
kumaransabapathy <31533930+kumaransabapathy@users.noreply.github.com>
2018-01-19 11:03:30 +0000

    Create yjhdf.c

---
+++ yjhdf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int j,a,sum=0;
+printf("enter the number");
+scanf("%d",&a);
+for(j=1;j<=a;j++)
+{
+sum=sum+j;
+}
+printf("sum of n natural numbers is");
+scanf("%d",&sum);
+}
+return 0;
+}
